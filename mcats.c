d84245836683f6e7d556fcf344fa20b9934dbbcb
Adrien Guinet <adrien.guinet@telecom-bretagne.eu>
2012-07-17 19:28:45 +0200

    GPLv3 licensing

--- mcats.c
+++ mcats.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of mcats.
+ * 
+ * Copyright 2012 Adrien Guinet <adrien@guinet.me>
+ * 
+ * mcats is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * mcats is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with mcats.  If not, see <http://www.gnu.org/licenses/>. 2
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
